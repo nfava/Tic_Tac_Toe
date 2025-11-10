c051fdcae51ea59fea94526acc83ee2d5e284112
nickf <nickfava006@gmail.com>
2025-11-10 08:52:26 -0600

    Working on logic, may not work

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -1,3 +1,4 @@
+
 def print_board(board):
 
     print("\n")
@@ -7,3 +8,19 @@ def print_board(board):
     print("---+---+---")
     print(f" {board[6]} | {board[7]} | {board[8]} ")
     print("\n")
+
+def check_winner(board, player):
+
+    win_conditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Columns
+        [0, 4, 8], [2, 4, 6]              # Diagonals
+    ]
+    return any(all(board[i] == player for i in combo) for combo in win_conditions)
+
+
+
+def tic_tac_toe():
+
+    if __name__ == "__main__":
+     tic_tac_toe()
